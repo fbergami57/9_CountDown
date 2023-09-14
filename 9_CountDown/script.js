b61f758a897191a20aded14e764fd1420e460fcf
Fabio Bergami <fabiobergami57@gmail.com>
2023-09-13 23:27:04 -0300

    Add a new file

--- 9_CountDown/script.js
+++ 9_CountDown/script.js
@@ -3,10 +3,29 @@ const hour = document.getElementById('hour')
 const minute = document.getElementById('minute')
 const second = document.getElementById('second')
 
-const launch = "17 oct 2023"
+const launch = "12 july 2024"
 
 function countDown() {
-    const datalaunch = new Date()
+    const dateLaunch = new Date(launch)
+    const today = new Date()
 
-    const segTotal = data
+    const secTotal = (dateLaunch - today)/1000;
+
+    const finalDays = Math.floor(secTotal / 60 / 60) / 24;
+    const finalHours = Math.floor(secTotal / 60 / 60) % 24;
+    const finalMinutes = Math.floor(secTotal / 60 ) % 60;
+    const finalSeconds = Math.floor(secTotal) % 60;
+
+    day.innerHTML = finalDays
+    hour.innerHTML = timeFormat(finalHours)
+    minute.innerHTML = timeFormat(finalMinutes)
+    second.innerHTML = timeFormat(finalSeconds)
+
 }
+
+function timeFormat(time) {
+    return time < 10? `0${time}` : time;
+}
+
+countDown();
+setInterval(countDown, 1000)
